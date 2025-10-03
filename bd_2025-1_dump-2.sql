20fc9726e6e85546dfb6017740fdd752afd8fbfb
Karpa <jmmontesdoreyes@gmail.com>
2025-10-03 13:55:03 -0600

    Comentario funciona totalmente

--- bd_2025-1_dump-2.sql
+++ bd_2025-1_dump-2.sql
@@ -169,7 +169,6 @@ UNLOCK TABLES;
 --- Estructura generada manualmente para tabla `encuestacomentario`
 ---
 
-DROP TABLE IF EXISTS `encuestacomentario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `encuestacomentario`(
